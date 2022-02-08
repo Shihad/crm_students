e7425048afec6fd0f3f1922052453d1491c5c23a
Shihad <arkady.gilev@gmail.com>
2022-02-08 11:59:30 +0500

    add new deal functionality

--- app/templates/script.js
+++ app/templates/script.js
@@ -4,3 +4,10 @@ $(".page_item").click(function() {
   $(".page_item").removeClass("page_active");
   $(this).addClass("page_active");
 })
+
+let num=1;
+  function new_deals(){
+  $('<div class="deal"> Сделка '+num+'</div>').appendTo("#new_deals_column");
+  num++;
+  console.log(num)
+}
