e4986647784589d3001bfaf94a5c7e7d52a1ba9a
Минакова Злата <minakova_zlata@ws-k1.class-11.real-it>
2022-02-09 10:53:21 +0500

    drag and drop elements

--- app/templates/script.js
+++ app/templates/script.js
@@ -9,5 +9,38 @@ let num=1;
   function new_deals(){
   $('<div class="deal"> Сделка '+num+'</div>').appendTo("#new_deals_column");
   num++;
-  console.log(num)
+
+  $(function() {
+
+  	$('.deal').draggable(
+    {
+        containment: "#deals_container",
+        snap: ".deals_column",
+        snapMode: "top",
+        snapTolerance: 50
+      }
+    );
+
+  });
 }
+
+$('.deals_column').droppable({
+      drop: function(event, ui) {
+           ui.draggable.text("Оставлено")
+      }
+  });
+/*
+function prepare_deals() {
+  $('<div class="deal"> Документ '+num+'</div>').appendTo("#prepare_deals_column");
+  num++;
+}
+
+function working_deals() {
+  $('<div class="deal"> Сделка '+num+'</div>').appendTo("#working_deals_column");
+  num++;
+}
+
+function closed_deals() {
+  $('<div class="deal"> Сделка '+num+'</div>').appendTo("#closed_deals_column");
+  num++;
+}*/
